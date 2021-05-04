b93741ca5deb2e0246caf8b3bb024555366ccf1c
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2021-05-04 14:51:02 +0700

    update

--- Day25_PHP_CRUD/Codes_demo/crud/database.php
+++ Day25_PHP_CRUD/Codes_demo/crud/database.php
@@ -1,20 +1,48 @@
 <?php
 /**
  * crud/database.php
- * Kết nối CSDL, tạo ra biến kết nối để
- * nhúng vào chức năng khác
+ * Kết nối CSDL theo MySQLi, đc dùng cho các chức năng
+ * CRUD khác
+ * - Tạo CSDL: php1020e_crud
+CREATE DATABASE IF NOT EXISTS php1020e_crud
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+ * - Click CSDL vừa tạo, tạo bảng: products
+ * id: khóa chính
+ * name: VARCHAR (150)
+ * price: INT (11)
+ * avatar: lưu tên file ảnh upload, VARCHAR(150)
+ * created_at: thời gian tạo sp, tự động sinh, TIMESTAMP,
+ * DEFAULT CURRENT_TIMESTAMP
+* - Tạo CSDL: php1020e_crud
+CREATE DATABASE IF NOT EXISTS php1020e_crud
+CHARACTER SET utf8 COLLATE utf8_general_ci;
+ * - SQL tạo bảng:
+CREATE TABLE IF NOT EXISTS products(
+  id INT(11) AUTO_INCREMENT,
+  name VARCHAR(150),
+  price INT(11),
+  avatar VARCHAR(150),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+);
  */
-// Máy chủ chứa CSDL sẽ kết nối
+// crud/database.php
+// - Các hằng số kết nối CSDL
+// Máy chủ chứa CSDL
 const DB_HOST = 'localhost';
+// Username kết nối vào CSDL
 const DB_USERNAME = 'root';
+// Mật khẩu kết nối vào CSDL
 const DB_PASSWORD = '';
-const DB_NAME = 'php1220e2_crud';
+// Tên CSDL sẽ kết nối
+const DB_NAME = 'php1020e_crud';
+// Cổng kết nối vào CSDL
 const DB_PORT = 3306;
-// Khởi tạo kết nối:
-$connection = mysqli_connect(DB_HOST,
-    DB_USERNAME, DB_PASSWORD, DB_NAME,
-    DB_PORT);
+// - Kết nối CSDL theo cơ chế MySQLi
+$connection = mysqli_connect(DB_HOST, DB_USERNAME,
+    DB_PASSWORD, DB_NAME, DB_PORT);
 if (!$connection) {
   die('Lỗi kết nối: ' . mysqli_connect_error());
 }
-echo "<h2>Kết nối CSDL thành công</h2>";
+echo "<h2>Kết nối CSDL " . DB_NAME . " thành công</h2>";
+
--- Day25_PHP_CRUD/Codes_demo/crud/index.php
+++ Day25_PHP_CRUD/Codes_demo/crud/index.php
@@ -1,69 +1,90 @@
 <?php
 session_start();
-//index.php
 require_once 'database.php';
 /**
- * Demo chức năng CRUD - Create - Read - Update
- * - Delete
- * crud
- *     /index.php: R - hiển thị danh sách sp
- *     /create.php: C - Thêm mới sp
- *     /update.php: U - Cập nhật sp
- *     /detail.php: Chi tiết sp
- *     /database.php: Kết nối CSDL
- * Code Create trước
+ * Ứng dụng CRUD - Create Read Update Delete
+ * 4 chức năng nền tảng của bất cứ 1 website nào
+ * Cấu trúc thư mục: qly sản phẩm
+ * crud/
+ *     /database.php: cấu hình kết nối CSDL theo mySQli
+ *     /index.php: liệt kê sản phẩm
+ *     /create.php: form thêm mới sp
+ *     /update.php: form update sp
+ *     /detail.php: chi tiết sp
+ *     /delete.php: xử lý xóa sp
  */
-// Kết nối CSDL lấy tất cả sp trong bảng products
-// - Viết truy vấn:
-$sql_select_all = "SELECT * FROM products 
-ORDER BY created_at DESC";
-// - Thực thi truy vấn: với SELECT trả về obj,
-//với INSERT, UPDATE, DELETE boolean
-$obj_select_all = mysqli_query($connection,
+//crud/index.php
+?>
+
+<?php
+// Session dạng flash
+if (isset($_SESSION['success'])) {
+  echo $_SESSION['success'];
+  unset($_SESSION['success']);
+}
+// Session dạng flash, chứa lỗi nếu có
+if (isset($_SESSION['error'])) {
+  echo $_SESSION['error'];
+  unset($_SESSION['error']);
+}
+
+// Xử lý lấy dữ liệu từ bảng products
+// + Viết truy vấn: theo thứ tự giảm dần của ngày tạo
+$sql_select_all =
+    "SELECT * FROM products ORDER BY created_at DESC";
+// + Thực thi truy vấn
+$obj_result_all = mysqli_query($connection,
     $sql_select_all);
-// - Lấy mảng kết hợp
-$products = mysqli_fetch_all($obj_select_all,
+// + Lấy dữ liệu trả về dưới dạng mảng kết hợp
+$products = mysqli_fetch_all($obj_result_all,
     MYSQLI_ASSOC);
 echo "<pre>";
 print_r($products);
 echo "</pre>";
 ?>
-<a href="create.php">Thêm mới</a>
-<h2>Danh sách sp</h2>
+<a href="create.php">Thêm mới sp</a>
 <table border="1" cellspacing="0" cellpadding="8">
-  <tr>
-    <th>Id</th>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Avatar</th>
-    <th>Created_at</th>
-    <th></th>
-  </tr>
-  <?php foreach($products AS $product): ?>
-  <tr>
-    <td><?php echo $product['id']; ?></td>
-    <td><?php echo $product['name']; ?></td>
-    <td>
-      <?php echo number_format($product['price'])?>
-    </td>
-    <td>
-      <img src="uploads/<?php echo $product['avatar']?>"
-           height="100px" />
-    </td>
-    <td>
-<!--      29/04/2021 20:48:58-->
-      <?php echo date('d/m/Y H:i:s',
-          strtotime($product['created_at'])) ?>
-    </td>
-    <td>
-      <a
-  href="update.php?id=<?php echo $product['id']?>">Sửa</a> &nbsp; &nbsp;
-      <a
-  href="delete.php?id=<?php echo $product['id']?>"
- onclick="return confirm('Chắc chắn xóa ko?')">
-        Xóa
-      </a>
-    </td>
-  </tr>
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Avatar</th>
+        <th>Created_at</th>
+        <th></th>
+    </tr>
+  <?php foreach ($products AS $product): ?>
+      <tr>
+          <td><?php echo $product['id']; ?></td>
+          <td><?php echo $product['name']; ?></td>
+          <td>
+          <?php echo number_format($product['price']); ?>
+          </td>
+          <td>
+      <img src="uploads/<?php echo $product['avatar'] ?>"
+           height="80px"/>
+          </td>
+          <td>
+<!--              09-01-2021 20:52:11-->
+      <?php
+      $created_at = date('d-M-Y H:i:s',
+          strtotime($product['created_at']));
+      echo $created_at;
+      ?>
+          </td>
+          <td>
+              <?php
+              $url_detail = "detail.php?id=" . $product['id'];
+              $url_update = "update.php?id=" . $product['id'];
+              $url_delete = "delete.php?id=" . $product['id'];
+              ?>
+              <a href="<?php echo $url_detail; ?>">Chi tiết</a>
+              <a href="<?php echo $url_update; ?>">Sửa</a>
+              <a href="<?php echo $url_delete; ?>"
+                 onclick="return confirm('Muốn xóa ko?')">
+                  Xóa
+              </a>
+          </td>
+      </tr>
+<!-- Ctrl + Alt + L -->
   <?php endforeach; ?>
 </table>
