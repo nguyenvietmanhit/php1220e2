d1cf5a52937d3c05a97e7f379183332dcca8818f
nvmanh <nguyenvietmanhit@gmail.com>
2021-05-13 19:48:03 +0700

    update

--- Day28_OOP_PDO_CRUD_And_MVC/Codes_demo/crud/create.php
+++ Day28_OOP_PDO_CRUD_And_MVC/Codes_demo/crud/create.php
@@ -1,4 +1,58 @@
 <?php
+session_start();
+require_once 'database.php';
 /**
  * crud/create.php
+ * Chức năng thêm mới sản phẩm:
+ * products: id, name, price, created_at
  */
+// Xử lý submit form
+// + Debug:
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+// + Tạo biến chứa lỗi
+$error = '';
+// + Chỉ xử lý form nếu user submit form:
+if (isset($_POST['submit'])) {
+  // + Tạo biến trung gian
+  $name = $_POST['name'];
+  $price = $_POST['price'];
+  // + Validate form: nếu có lỗi thì đổ vào biến $error
+  // + Xử lý dữ liệu từ form chỉ khi ko có lỗi xảy ra
+  if (empty($error)) {
+    // Insert vào bảng products, cần sử dụng đối tượng $connection
+    // kết nối từ file database.php, cần nhúng file
+    // + Viết truy vấn sử dụng tham số để tránh lỗi bảo mật SQLInjection
+    $sql_insert = "INSERT INTO products(name,price) VALUES(:name,:price)";
+    // + Cbi đối tượng truy vấn: prepare
+    $obj_insert = $connection->prepare($sql_insert);
+    // + [Tùy chọn] Tạo mảng để truyền giá trị vào tham số cho câu truy vấn:
+    $inserts = [
+      ':name' => $name,
+      ':price' => $price
+    ];
+    // + Thực thi đối tượng truy vấn: execute
+    $is_insert = $obj_insert->execute($inserts);
+//    var_dump($is_insert);
+    //Test file
+    if ($is_insert) {
+      $_SESSION['success'] = 'Thêm sp thành công';
+      // Chuyển hướng
+      header('Location: index.php');exit();
+    } else {
+      $error = 'Thêm sp thất bại';
+    }
+  }
+}
+?>
+<h3 style="color: red"><?php echo $error; ?></h3>
+<form action="" method="post">
+  Nhập tên sp:
+  <input type="text" name="name" value="" />
+  <br />
+  Nhập giá sp:
+  <input type="number" name="price" value="" />
+  <br />
+  <input type="submit" name="submit" value="Lưu" />
+</form>
--- Day28_OOP_PDO_CRUD_And_MVC/Codes_demo/crud/index.php
+++ Day28_OOP_PDO_CRUD_And_MVC/Codes_demo/crud/index.php
@@ -1,5 +1,10 @@
 <?php
+session_start();
+require_once 'database.php';
 /**
+ * crud/index.php
+ * Liệt kê sản phẩm
+ *
  * Demo CRUD sản phẩm đơn giản sử dụng PHP kết nối với CSDL MySQL bằng cơ chế PDO
  * Tạo cấu trúc thư mục như sau:
  * crud /
@@ -10,3 +15,35 @@
  *      / detail.php: xem chi tiết sp
  *      / delete.php: xóa sp
  */
+// Tương tác với CSDL để lấy ra các sp đang có trong bảng products
+// + Viết truy vấn dạng tham số nếu có, lấy theo giảm dần của ngày tạo
+$sql_select_all = "SELECT * FROM products ORDER BY created_at DESC";
+// + Cbi đối tượng truy vấn: prepare
+$obj_select_all = $connection->prepare($sql_select_all);
+// + [Tùy chọn] Tạo mảng để truyền giá trị -> bỏ qua vì câu truy vấn ko có tham số
+// + Thực thi obj truy vấn: execute
+$obj_select_all->execute();
+// + Lấy dữ liệu dưới dạng mảng kết hợp:
+
+?>
+<a href="create.php">THêm mới sp</a>
+<table border="1" cellspacing="0" cellpadding="6">
+  <tr>
+    <th>Id</th>
+    <th>Name</th>
+    <th>Price</th>
+    <th>Created_at</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>Name 3</td>
+    <td>12,000 vnđ</td>
+    <td>13/05/2020 12:34:50</td>
+    <td>
+      <a href="#">Xem</a>
+      <a href="#">Sửa</a>
+      <a href="#" onclick="return confirm('Chắc chắn muốn xóa?')">Xóa</a>
+    </td>
+  </tr>
+</table>
