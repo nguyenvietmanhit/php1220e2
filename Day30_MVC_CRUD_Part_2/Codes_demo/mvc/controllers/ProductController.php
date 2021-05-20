f1d8cae82d6dd5040fd1932dfaae30444fb62916
Nguyen Viet Manh <nvmanh@cmc.com.vn>
2021-05-20 10:02:58 +0700

    update

---
+++ Day30_MVC_CRUD_Part_2/Codes_demo/mvc/controllers/ProductController.php
@@ -0,0 +1,11 @@
+<?php
+//controllers/ProductController.php
+// Controller xử lý cho đối tượng product
+// Thử chạy lại url ban đầu xem có lỗi khác ko ?
+// index.php?controller=product&action=create
+class ProductController {
+  public function create() {
+    echo "mô hình mvc cho chức năng tạo mới sp đã chạy";
+  }
+}
+//
---
+++ Day30_MVC_CRUD_Part_2/Codes_demo/mvc/views/products/update.php
@@ -0,0 +1,2 @@
+<?php
+//update.php
