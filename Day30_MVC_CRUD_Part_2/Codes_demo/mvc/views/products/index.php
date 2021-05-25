06228815c384531726a6a1f0928dcf1f82c70b8d
nvmanh <nguyenvietmanhit@gmail.com>
2021-05-25 19:21:18 +0700

    update

--- Day30_MVC_CRUD_Part_2/Codes_demo/mvc/views/products/index.php
+++ Day30_MVC_CRUD_Part_2/Codes_demo/mvc/views/products/index.php
@@ -2,32 +2,37 @@
 //views/products/index.php
 // View của trang liệt kê sp
 // Thử debug biến $products sinh ra từ controller tại view này xem view có hiểu đc biến này ko
-echo "<pre>";
-print_r($products);
-echo "</pre>";
+//echo "<pre>";
+//print_r($products);
+//echo "</pre>";
 // Foreach mảng này ra table
 ?>
 <a href="index.php?controller=product&action=create">Thêm mới sp</a>
 <table border="1" cellspacing="0" cellpadding="8">
-  <tr>
-    <th>Id</th>
-    <th>Name</th>
-    <th>Price</th>
-    <th>Created_at</th>
-    <th></th>
-  </tr>
-  <tr>
-    <td>2</td>
-    <td>Name 2</td>
-    <td>20,000 vnđ</td>
-    <td>20/05/2021 21:25:50</td>
-    <td>
-      <a href="index.php?controller=product&action=detail&id=2">Chi tiết</a>
-      <a href="index.php?controller=product&action=update&id=2">Sửa</a>
-      <a href="index.php?controller=product&action=delete&id=2"
-         onclick="return confirm('Bạn muốn xóa ?')">
-          Xóa
-      </a>
-    </td>
-  </tr>
+    <tr>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Price</th>
+        <th>Created_at</th>
+        <th></th>
+    </tr>
+  <?php foreach ($products as $product): ?>
+      <tr>
+          <td><?php echo $product['id']; ?></td>
+          <td><?php echo $product['name']; ?></td>
+          <td><?php echo number_format($product['price']); ?> vnđ</td>
+          <td>
+<!--              20/05/2021 21:25:50-->
+              <?php echo date('d/m/Y H:i:s', strtotime($product['created_at'])); ?>
+          </td>
+          <td>
+              <a href="index.php?controller=product&action=detail&id=<?php echo $product['id']; ?>">Chi tiết</a>
+              <a href="index.php?controller=product&action=update&id=<?php echo $product['id']; ?>">Sửa</a>
+              <a href="index.php?controller=product&action=delete&id=<?php echo $product['id']; ?>"
+                 onclick="return confirm('Bạn muốn xóa ?')">
+                  Xóa
+              </a>
+          </td>
+      </tr>
+  <?php endforeach; ?>
 </table>
--- Day30_MVC_CRUD_Part_2/Codes_demo/mvc/views/products/update.php
+++ Day30_MVC_CRUD_Part_2/Codes_demo/mvc/views/products/update.php
@@ -1,2 +1,19 @@
 <?php
-//update.php
+//views/products/update.php
+// Form cập nhật sản phẩm: tận dụng lại HTML của form thêm mới
+// Copy nội dung file create.php
+?>
+<h1>Form cập nhậtr sp</h1>
+<form action="" method="post">
+  Nhập tên sp:
+  <input type="text" name="name" value="" />
+  <br />
+  Nhập giá sp:
+  <input type="number" name="price" value="" />
+  <br />
+  <input type="submit" name="submit" value="Lưu" />
+  <!--  Mọi link trong MVC đều phải tuân theo quy tắc đã đặt ra: url phải có 2 tham số controller và action  -->
+  <a href="index.php?controller=product&action=index">Về trang danh sách</a>
+</form>
+
+
